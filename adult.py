61005a9acf2f5ea57001c243cdbf8b94c165e587
kimyouumin <kimyouumin@gmail.com>
2026-05-13 19:52:44 +0900

    Update adult.py

--- adult.py
+++ adult.py
@@ -378,7 +378,7 @@ def render_adj_page():
     apply_adj_css()
     st.markdown("<br>", unsafe_allow_html=True)
     
-    st.markdown("<h2 style='text-align: center; color: #FFFFFF;'>단어를 골라주세요 ✨</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: #FFFFFF;'>✨스물에 가장 걸맞는다고 생각되는 형용사를 골라주세요 ✨</h2>", unsafe_allow_html=True)
     st.write("---")
     
     cols = st.columns(3)
